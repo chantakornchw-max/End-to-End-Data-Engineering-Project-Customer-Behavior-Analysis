6dad31f2eb822d9042bdc9df2c9102ee772c039b
flukchantakorn <chantakorn.chw@gmail.com>
2026-07-09 14:02:24 +0700

    Add service account for Cloud Composer

--- terraform/composer.tf
+++ terraform/composer.tf
@@ -1,4 +1,18 @@
 # ==========================================
+# Creat Service Account for Composer
+# ==========================================
+resource "google_service_account" "composer_sa" {
+  account_id   = "composer-worker-sa"
+  display_name = "Service Account for Cloud Composer"
+}
+
+resource "google_project_iam_member" "composer_worker" {
+  project = var.gcp_project_id
+  role    = "roles/composer.worker"
+  member  = "serviceAccount:${google_service_account.composer_sa.email}"
+}
+
+# ==========================================
 # Cloud Composer 2 (Managed Airflow)
 # ==========================================
 resource "google_composer_environment" "ecommerce_airflow" {
@@ -8,10 +22,19 @@ resource "google_composer_environment" "ecommerce_airflow" {
   config {
     environment_size = "ENVIRONMENT_SIZE_SMALL"
 
+    node_config {
+      service_account = google_service_account.composer_sa.email
+    }
+
     software_config {
       image_version = "composer-2-airflow-2"
     }
   }
+
+  depends_on = [
+    google_project_iam_member.composer_worker
+  ]
+
 }
 
 # ==========================================
